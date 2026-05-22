02203b5ccc9c54feecba964a3c0b667df138fe50
KLGROUP\181100.YUDISTIRA <yudistira.sujarwo@kawanlamacorp.com>
2026-05-22 14:30:10 +0700

    chore: remove grpc

--- lib/opentelemetry_logging.dart
+++ lib/opentelemetry_logging.dart
@@ -2,6 +2,5 @@
 library;
 
 export 'src/backend/open_telemetry_backend.dart';
-export 'src/backend/open_telemetry_grpc_backend.dart';
 export 'src/backend/open_telemetry_http_backend.dart';
 export 'src/open_telemetry_logger.dart';
